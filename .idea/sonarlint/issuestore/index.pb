82c11316958585fe4091096f1d4592bfec3f13ff
qxz0d8l <Sebastian.Bydalek@nttdata.com>
2019-07-21 16:24:07 +0200

    First commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+i
+9src/com/sebastianbydalek/todolist/datamodel/TodoItem.java,2\9\2938127cabeaf3e93c468942c242860cc4e04f63
+[
++src/com/sebastianbydalek/todolist/Main.java,c\4\c43b2def5dad96ac475c3677608b9cc27531ecfe
+g
+7src/com/sebastianbydalek/todolist/DialogController.java,c\e\cedcd5497e1710cb0c9a2b19e07b5215baa8446a
+a
+1src/com/sebastianbydalek/todolist/Controller.java,f\7\f7adbc52867442245b6c0b12940fe9880d8b1a65
+i
+9src/com/sebastianbydalek/todolist/datamodel/TodoData.java,7\3\73ef3666e80cd40177116940e462389da4bba5fd
+a
+1src/com/sebastianbydalek/todolist/mainwindow.fxml,c\1\c10ab774800559dd761af564def58cd8ae840b03
+e
+5src/com/sebastianbydalek/todolist/todoItemDialog.fxml,8\a\8a6e5e3ecde1653a463a8815d60fc12534f627f2
---
+++ src/com/sebastianbydalek/todolist/Controller.java
@@ -0,0 +1,83 @@
+package com.sebastianbydalek.todolist;
+
+import com.sebastianbydalek.todolist.datamodel.TodoData;
+import com.sebastianbydalek.todolist.datamodel.TodoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+
+public class Controller {
+    private List<TodoItem>todoItems;
+    @FXML
+    private ListView<TodoItem> todoListView;
+    @FXML
+    private TextArea itemDetailsTextArea;
+    @FXML
+    private Label deadLineLabel;
+    @FXML
+    private BorderPane mainBorderPane;
+
+    public void initialize(){
+
+
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends TodoItem> observable, TodoItem oldValue, TodoItem newValue) {
+                if(newValue !=null){
+                    TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                    itemDetailsTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadLineLabel.setText(df.format(item.getDeadLine()));
+                }
+            }
+        }  );
+
+
+        todoListView.setItems(TodoData.getInstance().getTodoItems());
+        todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
+    }
+    @FXML
+    public void showNewItemDialog() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        dialog.setTitle("Add new todo Item");
+        dialog.setHeaderText("Header Text ");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("todoItemDialog.fxml"));
+        try {
+
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+
+        } catch (IOException e) {
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            DialogController controller = fxmlLoader.getController();
+            TodoItem newItem = controller.processResults();
+            todoListView.getSelectionModel().select(newItem);
+        }
+    }
+    @FXML
+    public void handleClickListView(){
+        TodoItem item =  todoListView.getSelectionModel().getSelectedItem();
+        itemDetailsTextArea.setText(item.getDetails());
+        deadLineLabel.setText(item.getDeadLine().toString());
+
+
+    }
+}
